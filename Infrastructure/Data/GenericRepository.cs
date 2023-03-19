4179b977ec98a230123634fa9488d986f4d26e42
bilal <bilalsdd94@gmail.com>
2023-03-19 20:10:07 +0500

    +pagination,+filtering,+sorting,+Cors

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -38,6 +38,11 @@ namespace Infrastructure.Data
             throw new NotImplementedException();
         }
 
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+        
         private IQueryable<T> ApplySpecification(ISpecification<T> spec )
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(),spec);
