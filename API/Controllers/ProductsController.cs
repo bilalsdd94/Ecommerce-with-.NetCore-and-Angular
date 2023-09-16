124d4fa9bc92b564a0d4cd09319cc9db9892cea0
bilal <bilalsdd94@gmail.com>
2023-09-16 13:08:18 +0500

    cache on controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -32,7 +32,7 @@ namespace API.Controllers
             _productsRepo = productsRepo;
 
         }
-
+        [Cached(600)]
         [HttpGet]
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
             [FromQuery]ProductSpecParams productParams){
@@ -48,7 +48,7 @@ namespace API.Controllers
             
             return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,productParams.PageSize,totalItems,data));
         }
-
+        [Cached(600)]
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]        
         [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
@@ -60,14 +60,15 @@ namespace API.Controllers
 
             return _mapper.Map<Product,ProductToReturnDto>(product);
         }
-        
+
+        [Cached(600)]
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
             return Ok(await _productBrandRepo.ListAllAsync());    
             
         }
-
+        [Cached(600)]
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes(){
              return Ok(await _productTypeRepo.ListAllAsync()); 
